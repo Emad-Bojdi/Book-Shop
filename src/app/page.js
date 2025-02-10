2b167b6d0e26df9c07bf0d54432bd761e776d62d
Emaad <emadfredev@gmail.com>
2025-02-10 22:38:35 +0330

    Created 404 page for better UI and UX

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import NotFound from "@/components/modules/404";
 import HomePage from "@/components/template/HomePage";
 import SignUp from "@/components/template/SignUp";
 
@@ -5,7 +6,8 @@ import SignUp from "@/components/template/SignUp";
 export default function Home() {
   return (
     <>
-    <HomePage/>
+    {/* <HomePage/> */}
+    <NotFound/>
     </>
   );
 }
--- src/components/modules/404.js
+++ src/components/modules/404.js
@@ -3,13 +3,13 @@ import Image from "next/image"
 
 const NotFound = () => {
   return (
-    <div className="w-full flex flex-row justify-center">
-      <div classname="w-6/10">
-         <h2> پیدا نشد! </h2>
-         <p className="">
+    <div className="w-full h-screen flex flex-row justify-center items-center">
+      <div classname="w-6/10 h-screen justify-between">
+         <h2 className="font-vazir-bold text-center text-[40px]"> پیدا نشد! </h2>
+         <p className="font-vazir-normal text-[28px]">
             اطلاعات مورد نظر یافت نشد . احتمالا اشتباهی آمده اید!!
          </p>
-         <Image src="src" alt="alt" width={40} height={40} /> 
+         <Image src="/images/404.png" className="relative right-[100px]"  alt="alt" width={400} height={400} /> 
       </div>
     </div>
   )
