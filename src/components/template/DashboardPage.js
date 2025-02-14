8a629754ee7a96f51bf44923a6834ab728c80ae1
Emaad <emadfredev@gmail.com>
2025-02-14 19:04:04 +0330

    Craeted Dashboard page

--- src/components/template/DashboardPage.js
+++ src/components/template/DashboardPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SearchBar from '../modules/SearchBar'
 import Image from 'next/image'
+import TableList from './TableList'
 
 const DashboardPage = () => {
   return (
     <div className='bg-[#F7F8F8] w-full h-screen '>
       <SearchBar />
-      <div className='flex justify-center  mt-[20px]'>
+      <div className='flex justify-center  mt-[30px]'>
         <div className='w-4/5 flex justify-between items-center'>
           <div className='flex items-center flex-row space-x-[5px]'>
             <Image src={"icons/setting-3.svg"} alt='manage' width={30} height={30} />
@@ -19,7 +20,7 @@ const DashboardPage = () => {
           </div>
         </div>
       </div>
-      
+      <TableList/>
     </div>
   )
 }
--- src/components/template/TableList.js
+++ src/components/template/TableList.js
@@ -1,11 +1,37 @@
+import Image from "next/image";
 
 
 const TableList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="w-full flex justify-center mt-[25px]">
+            <table className="w-4/5 ">
+                <thead className="flex h-[60px] w-full bg-[#E4E4E4] rounded-t-[20px]">
+                    <tr className="w-9/10 h-full flex flex-row items-center  ">
+                        <th className="basis-1/4 font-vazir-medium text-[20px] leading-[31.25px] text-[#282828] "> نام کتاب </th>
+                        <th className="basis-1/4  font-vazir-medium text-[20px] leading-[31.25px] text-[#282828] "> موجودی </th>
+                        <th className="basis-1/4  font-vazir-medium text-[20px] leading-[31.25px] text-[#282828] "> قیمت </th>
+                        <th className="basis-1/4  font-vazir-medium text-[20px] leading-[31.25px] text-[#282828] "> شناسه کالا </th>
+                    </tr>
+                </thead>
+                <tbody className="flex w-full h-[40px] bg-[#FFFFFF]">
+                    <tr className="w-[1150px] h-full flex flex-row items-center ">
+                       <td className="basis-1/4 text-center font-vazir-normal text-[14px] leading-[21.88px] text-[#282828] ">جنایت و مکافات </td> 
+                       <td className="basis-1/4 text-center font-vazir-normal text-[13px] leading-[20.31px] text-[#282828] "> 293 </td> 
+                       <td className="basis-1/4 text-center font-vazir-normal text-[13px] leading-[20.31px] text-[#282828] "> 90 هزار تو</td> 
+                       <td className="basis-1/4 text-center font-vazir-normal text-[13px] leading-[20.31px] text-[#282828] ">jhjfbhjfavbjkfnrfe</td> 
+                       <td className="flex flex-row justify-self-end">
+                            <button className="border-none outline-none bg-inherit flex items-center justify-center">
+                                <Image src={"/icons/edit.svg"} alt="edit" width={20} height={20}/>
+                            </button>
+                            <button className="border-none outline-none bg-inherit flex items-center justify-center">
+                                <Image src={"/icons/trash.svg"} alt="edit" width={20} height={20}/>
+                            </button>
+                        </td> 
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default TableList;
